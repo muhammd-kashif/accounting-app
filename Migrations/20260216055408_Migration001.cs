820e7b069429c4581d49b46b206eda932e75aeb8
Sajjal Ahmed <sajjalahmed5@gmail.com>
2026-02-16 18:01:48 +0500

    income

---
+++ Migrations/20260216055408_Migration001.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountingApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPaid",
+                table: "Incomes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPaid",
+                table: "Incomes");
+        }
+    }
+}
